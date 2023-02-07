45633dc5b83aba9b03e61763801667498444d127
cmstiver <chayse.stiver@gmail.com>
2023-02-07 07:17:36 -0800

    fixed bug

--- Week 1/Day 1/algo-deaf-grandma/deaf_grandma.py
+++ Week 1/Day 1/algo-deaf-grandma/deaf_grandma.py
@@ -1,20 +1,20 @@
 def deaf_grandma():
-    goodbye = False
     print("HEY KID!")
-    while goodbye == False:
+    goodbyeCounter = 0
+    while True:
         answer = input("> ")
         if answer == "GOODBYE!":
-            print("LEAVING SO SOON?")
-            goodbyeAnswer = input("> ")
-            if goodbyeAnswer == "GOODBYE!":
+            goodbyeCounter += 1
+            if goodbyeCounter >= 2:
                 print("LATER, SKATER!")
                 break
-        if answer.upper() != answer:
+            print("LEAVING SO SOON?")
+        elif answer.upper() != answer:
             print("SPEAK UP, KID!")
+            goodbyeCounter = 0
         else:
             print("NO, NOT SINCE 1946!")
-
-
+            goodbyeCounter = 0
 
 
 deaf_grandma()
